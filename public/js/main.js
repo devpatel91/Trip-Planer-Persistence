13a8f4dc8104c94bc00d1d7bbd0a94011220aee0
devpatel91 <devnitinpatel91@gmail.com>
2016-08-18 16:10:37 -0400

    change

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,39 @@
 /* global $ tripModule */
 
 $(tripModule.load);
+
+
+$(function(){
+
+console.log("hello");
+
+
+$.get('/api/hotels')
+.then(function (hotels) {
+  hotels.forEach(function(hotel){
+    var $option = $('<option value= "' + hotel.id + '">' + hotel.name + '</option>');
+ $('#hotel-choices').append($option);
+  });
+})
+.catch( console.error.bind(console) );
+
+$.get('/api/restaurants')
+.then(function (restaurants) {
+  restaurants.forEach(function(restaurant){
+    var $option = $('<option value= "' + restaurant.id + '">' + restaurant.name + '</option>');
+ $('#restaurant-choices').append($option);
+  });
+})
+.catch( console.error.bind(console) );
+
+$.get('/api/activities')
+.then(function (activities) {
+  activities.forEach(function(activity){
+    var $option = $('<option value= "' + activity.id + '">' + activity.name + '</option>');
+ $('#activity-choices').append($option);
+  });
+})
+.catch( console.error.bind(console) );
+
+})
+
